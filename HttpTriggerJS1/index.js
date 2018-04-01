891fd235a20110bbacc92f0a13fc961b97cccf98
Jonathan Tougas <jtougas@gmail.com>
2018-04-01 16:25:07 -0400

    async/await that stuff

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,60 +1,49 @@
+const nodemailer = require("nodemailer");
 
-const nodemailer = require('nodemailer');
+async function asyncAzureFn(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+  context.log("nodemailer", nodemailer);
 
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.log('nodemailer', nodemailer);
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let account = await nodemailer.createTestAccount();
 
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    nodemailer.createTestAccount((err, account) => {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: account.user, // generated ethereal user
-                pass: account.pass // generated ethereal password
-            }
-        });
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: account.user, // generated ethereal user
+      pass: account.pass // generated ethereal password
+    }
+  });
 
-        // setup email data with unicode symbols
-        let mailOptions = {
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            to: 'jtougas@gmail.com', // list of receivers
-            subject: 'Hello ✔', // Subject line
-            text: 'Hello world?', // plain text body
-            html: '<b>Hello world?</b>' // html body
-        };
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "jtougas@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  };
 
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                context.log(error);
-                context.done();
-                return;
-            }
-            context.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
-            context.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  let info = await transporter.sendMail(mailOptions);
+  context.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  context.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
 
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-            if (req.query.name || (req.body && req.body.name)) {
-                context.res = {
-                    // status: 200, /* Defaults to 200 */
-                    body: {message: "Hello " + (req.query.name || req.body.name)}
-                };
-            }
-            else {
-                context.res = {
-                    status: 400,
-                    body: "Please pass a name on the query string or in the request body"
-                };
-            }          
-            context.done();    
-        });
+function azureFn(context, req) {
+  asyncAzureFn(context, req)
+    .then(function() {
+      context.log("Finished succesfully");
+      context.done();
+    })
+    .catch(function(err) {
+      context.log("Finished with error", err);
+      context.done();
     });
-};
+}
+
+module.exports = azureFn;
--- README.md
+++ README.md
@@ -1,11 +1,30 @@
-# Azure Functions quickstart starter sample 
+* https://portal.azure.com/
+* https://jt-function-test.azurewebsites.net/
+* https://jt-function-test.scm.azurewebsites.net
+* https://jt-function-test.azurewebsites.net/api/HttpTriggerJS1
 
-The code in the this repository supports the Azure Functions quickstart topic [Create your first function using the Azure CLI](https://docs.microsoft.com/azure/azure-functions/functions-create-first-azure-function-azure-cli). There are three HTTP triggered functions, one each for C#, F#, and JavaScript. These functions are generated from the same HTTP triggered function templates available in the Azure portal. 
+Run Azure functions locally:
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local
 
-## Contributing
+install dotnet 2.0 ->  https://github.com/dotnet/core/blob/master/release-notes/download-archives/2.0.0-download.md
 
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+```
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+```
 
-https://jt-function-test.azurewebsites.net/
-https://jt-function-test.scm.azurewebsites.net
-https://jt-function-test.azurewebsites.net/api/HttpTriggerJS1
+install nvm
+```
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+```
+
+install node v8.9.4:
+```
+nvm install 8.9.4
+```
+
+run the damn thing:
+```
+func host start
+```
